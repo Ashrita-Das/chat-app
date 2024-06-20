47a3c7034dcf5e11f2a6634dd1a3d80e5688ddc9
Ashrita-Das <ashrita315@gmail.com>
2024-06-21 01:02:55 +0530

    initialized project

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(5000, () => console.log("Server running on port 5000..."));
+dotenv.config();
+const port = process.env.port || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Hello World!");
+})
+
+app.listen(port, () => console.log(`Server running on port ${port}...`));
