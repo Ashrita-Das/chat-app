ee3d2d55bc26b528fceb1970b4f83c2946309dad
Ashrita-Das <ashrita315@gmail.com>
2024-06-22 00:59:13 +0530

    backend structure added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const port = process.env.port || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-})
+dotenv.config();
 
-app.listen(port, () => console.log(`Server running on port ${port}...`));
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+// })
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(port, () => {
+    connectToMongoDB();
+    console.log(`Server running on port ${port}...`)
+});
